3b250d2349d261f285b97602c6cfc1fbcde06063
Daniel <danielstoica078@gmail.com>
2024-01-08 21:42:50 +0000

    added the save option

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
     const currentDate = dayjs();
     $("#currentDay").text(currentDate.format("D MMM YYYY HH:mm:ss"));
 
-$(document).ready(function () {
-    loadSavedData();
+    $(document).ready(function () {
+        loadSavedData();
+
+    $(".saveBtn").on("click", function () {
+        const timeBlock = $(this).closest(".time-block");
+        const hour = timeBlock.find(".hour").text();
+        const description = timeBlock.find(".description").val();
+        saveToLocalStorage(hour, description);
+        });
